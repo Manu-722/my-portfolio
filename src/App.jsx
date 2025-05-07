489f163717669166f825a39fb1b275fd96876e82
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-05-07 23:35:02 +0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";  // Navbar in Components
-import Home from "./Components/Home";      // Now from Components folder
-import About from "./Components/About";    // Now from Components folder
-import Contacts from "./Components/Contacts"; // Now from Components folder
-import "./App.css"; // Uses App.css for styling
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contacts from "./Components/Contacts";
+import ThemeSwitcher from "./Components/ThemeSwitcher";
+import { ThemeProvider } from "./Components/ThemeContext";
+import "./index.css";
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
+    <ThemeProvider>
+      <div className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white transition-all">
+        <Navbar />
+        <div className="flex justify-end p-4">
+          <ThemeSwitcher />
+        </div>
+        <div className="container mx-auto px-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
